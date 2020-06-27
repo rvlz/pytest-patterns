8e7bf4dceed50ce18f962050870d4f39fb6cc400
Ricardo Veloz <rick@rvlz.io>
2020-06-26 20:17:16 -0500

    move response registration to responses fixture

--- test_facts.py
+++ test_facts.py
@@ -3,47 +3,39 @@ import responses as _responses  # new (to use 'responses' identifier elsewhere)
 
 from facts import get_math_fact, get_many_math_facts
 
+single_fact_data = [(4, "4 is the first positive non-Fibonacci number.")]
+many_facts_data = [
+    (1, "1 is the multiplicative identity."),
+    (2, "2 is a primorial, as well as its own factorial."),
+    (3, "3 is the number of spatial dimensions we live in."),
+]
+
 
 @pytest.fixture
-def responses():
+def responses(data):
     """Set up responses fixture."""
     with _responses.RequestsMock() as rsps:
+        for number, fact in data:
+            rsps.add(
+                _responses.GET,
+                "http://numbersapi.com/%s/math" % number,
+                body=fact,
+                status=200,
+            )
         yield rsps
 
 
+@pytest.mark.parametrize("data", [single_fact_data])
 def test_get_math_fact(responses):
     """Test function returns a single math fact."""
-    responses.add(
-        _responses.GET,
-        "http://numbersapi.com/4/math",
-        body="4 is the first positive non-Fibonacci number.",
-        status=200,
-    )
     fact = get_math_fact(4)
     assert fact == "4 is the first positive non-Fibonacci number."
     assert len(responses.calls) == 1
 
 
+@pytest.mark.parametrize("data", [many_facts_data])
 def test_get_many_facts(responses):
     """Test function can get multiple math facts."""
-    responses.add(
-        _responses.GET,
-        "http://numbersapi.com/1/math",
-        body="1 is the multiplicative identity.",
-        status=200,
-    )
-    responses.add(
-        _responses.GET,
-        "http://numbersapi.com/2/math",
-        body="2 is a primorial, as well as its own factorial.",
-        status=200,
-    )
-    responses.add(
-        _responses.GET,
-        "http://numbersapi.com/3/math",
-        body="3 is the number of spatial dimensions we live in.",
-        status=200,
-    )
     numbers = [1, 2, 3]
     facts = get_many_math_facts(numbers)
     assert facts == [
